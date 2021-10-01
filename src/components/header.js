6ee89082d4afd776762da52099626d9379fd084c
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-01 11:23:58 -0700

    JS header complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,30 @@
+
 const Header = (title, date, temp) => {
-  // TASK 1
+  const header1 =document.createElement('div');
+  const date1 =document.createElement('span');
+  const title1 =document.createElement('h1');
+  const temperature1 =document.createElement('span');
+
+  header1.appendChild(date1);
+  header1.appendChild(title1);
+  header1.appendChild(temperature1);
+
+  header1.classList.add('header');
+  date1.classList.add('date');
+  temperature1.classList.add('temp');
+
+  date1.textContent=`${date}`;
+  title1.textContent=`${title}`;
+  temperature1.textContent=`${temp}`;
+
+  return header1
+
+}
+
+
+// console.log(Header)
+
+// TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
@@ -11,8 +36,6 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
-
 const headerAppender = (selector) => {
   // TASK 2
   // ---------------------
@@ -20,6 +43,8 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const selector1 = document.querySelector(`${selector}`)
+  selector1.appendChild(Header('title', 'date','temp'));
 }
 
 export { Header, headerAppender }
